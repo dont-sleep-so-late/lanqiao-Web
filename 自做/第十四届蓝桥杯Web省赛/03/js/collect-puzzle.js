e7b829b192f6691ece0fe3fff276d5e49d727dc4
dont-sleep-so-late <Imtiancai233@qq.com>
2024-04-02 10:40:51 +0800

    完成数字去重

--- 自做/第十四届蓝桥杯Web省赛/03/js/collect-puzzle.js
+++ 自做/第十四届蓝桥杯Web省赛/03/js/collect-puzzle.js
@@ -1,6 +1,8 @@
 function collectPuzzle(...puzzles) {
   // TODO:在这里写入具体的实现逻辑
   // 对所有的拼图进行收集，获取不同拼图类型的结果，并返回
+  // return Array.from(new Set(puzzles.flat()));
+  return [...new Set(puzzles.flat())];
 }
 
 // 检测需要，请勿删除
